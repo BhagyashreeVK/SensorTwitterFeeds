48fd0d7f32f465d60d5b697b0d29bcc5722087dd
BhagyashreeVK <bvkulkarni2512@gmail.com>
2016-03-26 15:54:38 -0400

    Removed unused Libraries

--- src/main/java/com/twitterfeeds/webservice/operations/TwitterWebService.java
+++ src/main/java/com/twitterfeeds/webservice/operations/TwitterWebService.java
@@ -3,22 +3,17 @@ package com.twitterfeeds.webservice.operations;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import com.twitterfeeds.javamain.AccessTwitterMain;
 import com.twitterfeeds.javamain.AuthKeys;
 import com.twitterfeeds.javamain.TweetUsingJava;
